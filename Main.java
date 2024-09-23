75c836a5d56f6aabf5d5205f0f095c5e5209cbae
LoganJeong0 <loganjeong0@gmail.com>
2024-09-23 16:21:59 +0000

    commit

--- Main.java
+++ Main.java
@@ -2,9 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-       // Feeder i = new Feeder(1000);
-       // i.simulateOneDay(5);
-       // System.out.println(i);
         Feeder j = new Feeder(1000);
         System.out.println(j.simulateManyDays(10, 10));
     }
